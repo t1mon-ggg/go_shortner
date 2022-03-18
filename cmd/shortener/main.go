b950ddbc5efda810692a4b0fef1157d0ffd76a50
t1mon-ggg <t1mon.ggg@yandex.ru>
2022-03-18 23:58:54 +0300

    static varian

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,47 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func MyHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		if len(r.RequestURI) == 1 {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("some_long_url"))
+		}
+		return
+	case http.MethodPost:
+		defer r.Body.Close()
+		blongURL, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		slongURL := string(blongURL)
+		log.Println(slongURL)
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("some_short_url"))
+		return
+	default:
+		log.Println(r)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+}
+
+func main() {
+	addr := "127.0.0.1:8080"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", MyHandler)
+	log.Println(fmt.Sprintf("Запуск сервера на %s", addr))
+	err := http.ListenAndServe(addr, mux)
+	if err != nil {
+		panic(err)
+	}
+}
